f95bedac698b7fa7d6b92668d7b0718350d3bb55
solify-dev <owner@solify-dev.org>
2025-09-25 23:33:48 +1000

    feat: fix static assets

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
+import { deployedUrl } from "@/lib/constants";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: `${deployedUrl}/cctpv2`,
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
   webpack(config) {
     config.externals.push("pino-pretty", "lokijs", "encoding");
